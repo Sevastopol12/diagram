9eb1b218298297343edf53b9a7ba812b70549ba7
Sevastopol <141613215+Sevastopol12@users.noreply.github.com>
2024-04-17 14:11:29 +0700

    Add files via upload

---
+++ mock.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import random as rnd
+
+class BinTree:
+    class Node:
+        def __init__(self, val):
+            self.val = val
+            self.right = None
+            self.left = None
+
+    def __init__(self, node, root):
+        self.node = self.Node
+        self.root = None
+
+    def traversal(self, node):
+        result = self.recursive_trv(self.root)
+
+    def recursive_trv(self, node):
+        if node is None: return
+
+        
+def main():
+    ...
+
+
+if __name__ == "__main__": main()
